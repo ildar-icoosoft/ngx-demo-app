07178dbc2899051a541753d62d95414c93a56b0d
Ildar Farrakhov <ildar.icoosoft@gmail.com>
2022-05-27 03:42:23 +0500

    fix: Исправил поведение фильтра на странице со списком постов
    Сделал следующее:
    1. При открытии страницы фильтр сбрасывается для того, чтобы не применялись старые фильтры
    2. При изменении фильтра устанавливается первая страницы

--- src/app/features/post/post-list/ngrx-store/post-list.reducer.ts
+++ src/app/features/post/post-list/ngrx-store/post-list.reducer.ts
@@ -2,12 +2,13 @@ import { PayloadAction } from '../../../../core/ngrx-store/types/payload-action'
 import * as postActions from '../../../../core/ngrx-store/actions/post.actions';
 import NormalizedData from '../../../../core/normalizr/types/normalized-data';
 import { PageResult } from '../../../../core/types/pagination/page-result';
+import { Pageable } from '../../../../core/types/pagination/pageable';
 
 export const postListFeatureKey = 'postList';
 
 export type PostListState = PageResult<number>;
 
-export const initialState: PostListState = {
+export const postListInitialPageable: Pageable = {
   page: {
     number: 1,
     size: 5,
@@ -16,12 +17,15 @@ export const initialState: PostListState = {
     field: 'id',
     direction: 'asc',
   },
+};
+export const postListInitialState: PostListState = {
+  ...postListInitialPageable,
   totalCount: 0,
   items: [],
 };
 
 // eslint-disable-next-line @typescript-eslint/default-param-last
-export function reducer(state = initialState, action: PayloadAction<any>): PostListState {
+export function reducer(state = postListInitialState, action: PayloadAction<any>): PostListState {
   switch (action.type) {
     case postActions.GET_POSTS_SUCCESS:
       const data = action.payload as NormalizedData<PageResult<number>>;
