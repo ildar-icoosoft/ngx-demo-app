9336c6f57e34ae39274af2a02a52f37f1ce83252
Ildar Farrakhov <ildar.icoosoft@gmail.com>
2022-05-30 02:40:59 +0500

    feat: Создал каркас страницы photo list (без данных)
    создал страницу со всеми компонентами. Страница пока полностью статичная: данные захардкожены, фильтрация не работает.

---
+++ src/app/features/photo/photo-list/components/filter/container/photo-list-filter-container.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-list-filter-container',
+  templateUrl: './photo-list-filter-container.component.html',
+  styleUrls: ['./photo-list-filter-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PhotoListFilterContainerComponent {}
